7dc966ca045057245a5908e1267756c006c91f03
ScottUK <scottukgaming@gmail.com>
2018-03-30 15:43:05 +0100

    Fixed timer loop issue and fixed /forcestart

--- DemolitionDerby/SERVER/Server.lua
+++ DemolitionDerby/SERVER/Server.lua
@@ -1,4 +1,5 @@
 -- Variables
+startingGame = false
 timermax = 121
 startTimer = 0
 
@@ -16,20 +17,30 @@ AddEventHandler('gameStartTimer', function()
 			startTimer = startTimer - 1
 			TriggerClientEvent('updateStartTimer', -1, startTimer)
 			Citizen.Wait(1000)
+			if startTimer == 1 then
+				startingGame = true
+				TriggerClientEvent('updateStartingGame', -1, startingGame)
+				TriggerClientEvent('startGame', -1)
+			end
 		end
 	elseif startTimer ~= 0 then
 		CancelEvent()
 	end
 end)
 
-RegisterServerEvent('startGame')
-AddEventHandler('startGame', function()
+RegisterServerEvent('forceStartGame')
+AddEventHandler('forceStartGame', function()
 	Citizen.Wait(1)
 	if startTimer > 0 then
 		while startTimer > 0 do
 			startTimer = startTimer - 1
 			TriggerClientEvent('updateStartTimer', -1, startTimer)
 			Citizen.Wait(100)
+			if startTimer == 1 then
+				startingGame = true
+				TriggerClientEvent('updateStartingGame', -1, startingGame)
+				TriggerClientEvent('startGame', -1)
+			end
 		end
 	end
 end)
