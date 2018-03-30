cbb2c2719af19d4497fa1591e9a826ca3064105a
Flatracer <Flatracer@ymail.com>
2018-03-30 15:38:56 +0200

    Temporary fix

--- DemolitionDerby/__resource.lua
+++ DemolitionDerby/__resource.lua
@@ -1,5 +1,3 @@
-resource_type 'gametype' { name = 'Demolition Derby' }
-
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Demolition Derby - A gamemode by Scotty & Flatracer'
