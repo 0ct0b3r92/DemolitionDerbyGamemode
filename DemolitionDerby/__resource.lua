856ff1520e2c8906ba59845144a70838c7ded70e
Flatracer <Flatracer@ymail.com>
2018-03-30 14:57:24 +0200

    Updated Folder Structure

--- DemolitionDerby/__resource.lua
+++ DemolitionDerby/__resource.lua
@@ -1,11 +1,14 @@
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+resource_type 'gametype' { name = 'Demolition Derby' }
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Demolition Derby - A gamemode by Scotty & Flatracer'
 
 client_script {
-	'client.lua',
+	'CLIENT/client.lua',
 }
 
 server_script {
-	'server.lua',
+	'SERVER/SlotReserving.lua',
+	'SERVER/server.lua',
 }
