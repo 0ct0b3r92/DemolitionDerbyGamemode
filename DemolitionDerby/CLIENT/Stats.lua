382993159cc7ae0615b7f4f6b4843a95605c25ab
ScottUK <scottukgaming@gmail.com>
2018-04-27 16:51:48 +0100

    Create Stats.lua

---
+++ DemolitionDerby/CLIENT/Stats.lua
@@ -0,0 +1,22 @@
+wins = 0;losses = 0;kills = 0
+
+AddEventHandler("playerSpawned", function()
+	TriggerServerEvent("DD:Server:DB:CheckAndInsert")
+	Citizen.Wait(2000)
+	TriggerServerEvent('DD:Server:UpdateWins')
+end)
+
+RegisterNetEvent('DD:Client:UpdateWins')
+AddEventHandler('DD:Client:UpdateWins', function(newWins)
+    wins = newWins
+end)
+
+RegisterNetEvent('DD:Client:UpdateLosses')
+AddEventHandler('DD:Client:UpdateLosses', function(newLosses)
+    losses = newLosses
+end)
+
+RegisterCommand('testy', function()
+	TriggerServerEvent('DD:Server:UpdateWins')
+	TriggerEvent('chatMessage', '', {255, 255, 255}, wins)
+end)
